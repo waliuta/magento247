c596a6a40df812e2992fd20366f2f53c5b4a39a7
waliuta <waliborman@gmail.com>
2024-09-04 17:30:17 +0300

    Получили первую категорию текущего подукта, в ней получили заданное количество продуктов с заданно разницей в цене

--- app/code/Perspective/ThreeProducts/ViewModel/ThreeProductsViewModel.php
+++ app/code/Perspective/ThreeProducts/ViewModel/ThreeProductsViewModel.php
@@ -8,18 +8,54 @@ class ThreeProductsViewModel implements \Magento\Framework\View\Element\Block\Ar
      */
     private $viewproduct;
 
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
+     */
+    private $productCollectionFactory;
+
     public function __construct(
 
-        \Magento\Catalog\Block\Product\View $viewproduct
+        \Magento\Catalog\Block\Product\View $viewproduct,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
     )
     {
 
         $this->viewproduct = $viewproduct;
+        $this->productCollectionFactory = $productCollectionFactory;
     }
 
     public function getCurrentProduct()
     {        
         return $this->viewproduct->getProduct();
+    }
+
+    public function getCurrentCategoryIds()
+    {        
+        return  $this->getCurrentProduct()->getCategoryIds();
+    }
+    
+
+    public function getCurrentFirstCategoryId()
+    {        
+        $categoryIds =  $this->getCurrentProduct()->getCategoryIds();
+         if (!empty($categoryIds)) {
+            // Берем первый ID из массива категорий
+            $firstCategoryId = $categoryIds[0];
+        return $firstCategoryId;
+    }
+    }
+
+    public function getProductsByCategoryId($currentProduct, $categoryId, $rate, $countProducts)
+    {
+        // Загружаем коллекцию продуктов по ID категории
+        $productCollection = $this->productCollectionFactory->create()
+            ->addAttributeToSelect('*') // Выбираем все атрибуты продуктов
+            ->addCategoriesFilter(['in' => $categoryId]) // Фильтр по ID категории
+            ->addFieldToFilter('price', ['from' => $currentProduct->getFinalPrice() - $rate, 'to' => $currentProduct->getFinalPrice() + $rate])
+            ->setPageSize($countProducts) // Ограничиваем коллекцию 3 продуктами
+            ->load();
+
+        return $productCollection;
     }
 
 }
